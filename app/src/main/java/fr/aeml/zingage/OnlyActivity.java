d71e635685c641f7cf755cd8187f09641c79989e
Virgile PARAT <virgile.parat@live.fr>
2017-06-09 10:08:08 +0200

    Enable temporary value storage - Disable Caching

--- app/src/main/java/fr/aeml/zingage/OnlyActivity.java
+++ app/src/main/java/fr/aeml/zingage/OnlyActivity.java
@@ -12,12 +12,22 @@ public class OnlyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_only);
+
+        //Declare Web View
         WebView zoneWeb = (WebView) findViewById(R.id.zoneWeb);
         zoneWeb .setWebViewClient(new WebViewClient());
         zoneWeb.getSettings().setLoadWithOverviewMode(true);
         zoneWeb.getSettings().setUseWideViewPort(true);
+
+        //Change Web View setting
         WebSettings webSettings = zoneWeb.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+
         zoneWeb.loadUrl("http://192.168.0.20/zingage/");
     }
 }
