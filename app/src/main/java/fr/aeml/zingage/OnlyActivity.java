26b64fc7224da1474b229a448bf73438066cc06d
Virgile PARAT <virgile.parat@live.fr>
2017-06-14 15:39:15 +0200

    Changement de la stratégie de sauvegarde du cache pour éviter des incompatibilité avec la gestion du dynamise sur PHP

--- app/src/main/java/fr/aeml/zingage/OnlyActivity.java
+++ app/src/main/java/fr/aeml/zingage/OnlyActivity.java
@@ -1,7 +1,13 @@
 package fr.aeml.zingage;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -21,17 +27,52 @@ public class OnlyActivity extends AppCompatActivity {
         WebSettings webSettings = zoneWeb.getSettings();
 
         //Alter settings Web View
-        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         webSettings.setAppCacheEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
         webSettings.setDomStorageEnabled(true);
         webSettings.setBuiltInZoomControls(false);
         webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
 
 
         zoneWeb.loadUrl("http://192.168.0.20/zingage/");
+
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieSyncManager.createInstance(zoneWeb.getContext());
+
+        /******************Side Bar*********************/
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.menuView);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem){
+                DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
+                WebView zoneWeb = (WebView) findViewById(R.id.zoneWeb);
+                switch (menuItem.getItemId()){
+                    case(R.id.depart):
+                        zoneWeb.loadUrl("http://192.168.0.20/zingage/depart");
+                        break;
+                    case(R.id.retour):
+                        zoneWeb.loadUrl("http://192.168.0.20/zingage/retour");
+                        break;
+                    case(R.id.article):
+                        zoneWeb.loadUrl("http://192.168.0.20/zingage/article");
+                        break;
+                    case(R.id.historique):
+                        zoneWeb.loadUrl("http://192.168.0.20/zingage/scan");
+                        break;
+                    case(R.id.profil):
+                        zoneWeb.loadUrl("http://192.168.0.20/zingage/profil");
+                        break;
+                }
+                mDrawerLayout.closeDrawer(Gravity.START);
+                return true;
+            }
+        });
     }
 }
+
+
+
