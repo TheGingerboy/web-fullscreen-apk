302209aede5e6472cd092162daf890590dff6ff1
Virgile PARAT <virgile.parat@live.fr>
2017-07-19 18:36:04 +0200

    Ajout d'une barre en haut et du bouton pour faire défiler le menu - More user friendly

--- app/src/main/java/fr/aeml/zingage/OnlyActivity.java
+++ app/src/main/java/fr/aeml/zingage/OnlyActivity.java
@@ -2,6 +2,7 @@ package fr.aeml.zingage;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -14,12 +15,26 @@ import android.webkit.WebViewClient;
 
 public class OnlyActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_only);
 
-        //Declare Web View
+        //declare DrawerLayout
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
+        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        /******************Webview*********************/
         WebView zoneWeb = (WebView) findViewById(R.id.zoneWeb);
         zoneWeb.setWebViewClient(new WebViewClient());
 
@@ -37,7 +52,7 @@ public class OnlyActivity extends AppCompatActivity {
         webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
 
 
-        zoneWeb.loadUrl("http://192.168.0.20/zingage/");
+        zoneWeb.loadUrl("http://192.168.25.1/zingage/");
 
         CookieManager.getInstance().setAcceptCookie(true);
         CookieSyncManager.createInstance(zoneWeb.getContext());
@@ -51,20 +66,29 @@ public class OnlyActivity extends AppCompatActivity {
                 DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
                 WebView zoneWeb = (WebView) findViewById(R.id.zoneWeb);
                 switch (menuItem.getItemId()){
+                    case(R.id.accueil):
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/");
+                        break;
                     case(R.id.depart):
-                        zoneWeb.loadUrl("http://192.168.0.20/zingage/depart");
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/depart");
                         break;
                     case(R.id.retour):
-                        zoneWeb.loadUrl("http://192.168.0.20/zingage/retour");
-                        break;
-                    case(R.id.article):
-                        zoneWeb.loadUrl("http://192.168.0.20/zingage/article");
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/retour");
                         break;
                     case(R.id.historique):
-                        zoneWeb.loadUrl("http://192.168.0.20/zingage/scan");
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/scan");
+                        break;
+                    case(R.id.article):
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/article");
                         break;
                     case(R.id.profil):
-                        zoneWeb.loadUrl("http://192.168.0.20/zingage/profil");
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/profil");
+                        break;
+                    case(R.id.administration):
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/administration");
+                        break;
+                    case(R.id.deconnexion):
+                        zoneWeb.loadUrl("http://192.168.25.1/zingage/deconnexion");
                         break;
                 }
                 mDrawerLayout.closeDrawer(Gravity.START);
@@ -72,6 +96,17 @@ public class OnlyActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        if(mToogle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
 
 
